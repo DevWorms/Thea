954dbc3890eb2f20f33442128db66011fcf48e96
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-10-17 01:58:12 -0500

    look and feel de la app movil

---
+++ app/src/main/java/com/devworms/salva/thea/FragmentPosicion.java
@@ -0,0 +1,300 @@
+package com.devworms.salva.thea;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+/**
+ * Created by sergio on 16/10/16.
+ */
+
+public class FragmentPosicion extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks,
+        LocationListener {
+
+    private static final String LOGTAG = "android-localizacion";
+
+    private static final int PETICION_PERMISO_LOCALIZACION = 101;
+    private static final int PETICION_CONFIG_UBICACION = 201;
+
+    private GoogleApiClient apiClient;
+
+    private TextView lblLatitud;
+    private TextView lblLongitud;
+    private ToggleButton btnActualizar;
+    String lat,lon;
+    private LocationRequest locRequest;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_ubicacion);
+
+
+        lblLatitud = (TextView) findViewById(R.id.lblLatitud);
+        lblLongitud = (TextView) findViewById(R.id.lblLongitud);
+        btnActualizar = (ToggleButton) findViewById(R.id.btnActualizar);
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleLocationUpdates(btnActualizar.isChecked());
+            }
+        });
+
+        //Construcción cliente API Google
+        apiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+
+    }
+
+
+
+
+    private void toggleLocationUpdates(boolean enable) {
+        if (enable) {
+            enableLocationUpdates();
+        } else {
+            disableLocationUpdates();
+        }
+    }
+
+    private void enableLocationUpdates() {
+
+        locRequest = new LocationRequest();
+        locRequest.setInterval(5000);
+        locRequest.setFastestInterval(5000);
+        locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest locSettingsRequest =
+                new LocationSettingsRequest.Builder()
+                        .addLocationRequest(locRequest)
+                        .build();
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        apiClient, locSettingsRequest);
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult locationSettingsResult) {
+                final Status status = locationSettingsResult.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+
+                        Log.i(LOGTAG, "Configuración correcta");
+                        startLocationUpdates();
+
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            Log.i(LOGTAG, "Se requiere actuación del usuario");
+                            status.startResolutionForResult(FragmentPosicion.this, PETICION_CONFIG_UBICACION);
+                        } catch (IntentSender.SendIntentException e) {
+                            btnActualizar.setChecked(false);
+                            Log.i(LOGTAG, "Error al intentar solucionar configuración de ubicación");
+                        }
+
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.i(LOGTAG, "No se puede cumplir la configuración de ubicación necesaria");
+                        btnActualizar.setChecked(false);
+                        break;
+                }
+            }
+        });
+    }
+
+    private void disableLocationUpdates() {
+
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                apiClient, this);
+
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(FragmentPosicion.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            //Ojo: estamos suponiendo que ya tenemos concedido el permiso.
+            //Sería recomendable implementar la posible petición en caso de no tenerlo.
+
+            Log.i(LOGTAG, "Inicio de recepción de ubicaciones");
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    apiClient, locRequest, FragmentPosicion.this);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        //Se ha producido un error que no se puede resolver automáticamente
+        //y la conexión con los Google Play Services no se ha establecido.
+
+        Log.e(LOGTAG, "Error grave al conectar con Google Play Services");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        //Conectado correctamente a Google Play Services
+
+       if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PETICION_PERMISO_LOCALIZACION);
+        } else {
+
+            Location lastLocation =
+                    LocationServices.FusedLocationApi.getLastLocation(apiClient);
+
+            updateUI(lastLocation);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //Se ha interrumpido la conexión con Google Play Services
+
+        Log.e(LOGTAG, "Se ha interrumpido la conexión con Google Play Services");
+    }
+
+    private void updateUI(Location loc) {
+        if (loc != null) {
+            lblLatitud.setText("Latitud: " + String.valueOf(loc.getLatitude()));
+            lblLongitud.setText("Longitud: " + String.valueOf(loc.getLongitude()));
+            lat=String.valueOf(loc.getLatitude());
+            lon= String.valueOf(loc.getLongitude());
+            new mandarLoc().execute();
+        } else {
+            lblLatitud.setText("Latitud: (desconocida)");
+            lblLongitud.setText("Longitud: (desconocida)");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PETICION_PERMISO_LOCALIZACION) {
+            if (grantResults.length == 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                //Permiso concedido
+
+                @SuppressWarnings("MissingPermission")
+                Location lastLocation =
+                        LocationServices.FusedLocationApi.getLastLocation(apiClient);
+
+                updateUI(lastLocation);
+
+            } else {
+                //Permiso denegado:
+                //Deberíamos deshabilitar toda la funcionalidad relativa a la localización.
+
+                Log.e(LOGTAG, "Permiso denegado");
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PETICION_CONFIG_UBICACION:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        startLocationUpdates();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i(LOGTAG, "El usuario no ha realizado los cambios de configuración necesarios");
+                        btnActualizar.setChecked(false);
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        Log.i(LOGTAG, "Recibida nueva ubicación!");
+
+        //Mostramos la nueva ubicación recibida
+        updateUI(location);
+    }
+
+
+    class mandarLoc extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        /**
+         * getting Albums JSON
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            //add your data
+            //add your data
+            JSONParse jsp= new JSONParse();
+
+            //String body= "{\r\n\"contrasena\": \""+txtPass.getText()+"\",\r\n\"telefono\": \""+txtTel.getText()+"\"\r\n}\r\n";
+            String body= "{\n\"id\" : \"2\",\n\"lat\" : \""+lat+"\",\n\"lon\" : \""+lon+"\"\n}";
+            String respuesta= jsp.makeHttpRequest("http://thea.devworms.com/api/usuarios/trackup","POST",body,"");
+            Log.d("Respuesta : ", "> " + respuesta);
+            if(respuesta!="error") {
+
+
+            }
+            else{}
+            return null;
+        }
+
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all albums
+
+
+        }
+    }
+}
--- app/src/main/java/com/devworms/salva/thea/MainActivity.java
+++ app/src/main/java/com/devworms/salva/thea/MainActivity.java
@@ -7,10 +7,18 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,259 +39,29 @@ import com.google.android.gms.location.LocationSettingsStatusCodes;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity
-        implements GoogleApiClient.OnConnectionFailedListener,
-        GoogleApiClient.ConnectionCallbacks,
-        LocationListener {
+public class MainActivity extends AppCompatActivity{
 
-    private static final String LOGTAG = "android-localizacion";
 
-    private static final int PETICION_PERMISO_LOCALIZACION = 101;
-    private static final int PETICION_CONFIG_UBICACION = 201;
-
-    private GoogleApiClient apiClient;
-
-    private TextView lblLatitud;
-    private TextView lblLongitud;
-    private ToggleButton btnActualizar;
-    String lat,lon;
-    private LocationRequest locRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lblLatitud = (TextView) findViewById(R.id.lblLatitud);
-        lblLongitud = (TextView) findViewById(R.id.lblLongitud);
-        btnActualizar = (ToggleButton) findViewById(R.id.btnActualizar);
-        btnActualizar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggleLocationUpdates(btnActualizar.isChecked());
-            }
-        });
-
-        //Construcción cliente API Google
-        apiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addConnectionCallbacks(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    private void toggleLocationUpdates(boolean enable) {
-        if (enable) {
-            enableLocationUpdates();
-        } else {
-            disableLocationUpdates();
-        }
-    }
-
-    private void enableLocationUpdates() {
-
-        locRequest = new LocationRequest();
-        locRequest.setInterval(5000);
-        locRequest.setFastestInterval(5000);
-        locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationSettingsRequest locSettingsRequest =
-                new LocationSettingsRequest.Builder()
-                        .addLocationRequest(locRequest)
-                        .build();
-
-        PendingResult<LocationSettingsResult> result =
-                LocationServices.SettingsApi.checkLocationSettings(
-                        apiClient, locSettingsRequest);
-
-        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
-            @Override
-            public void onResult(LocationSettingsResult locationSettingsResult) {
-                final Status status = locationSettingsResult.getStatus();
-                switch (status.getStatusCode()) {
-                    case LocationSettingsStatusCodes.SUCCESS:
-
-                        Log.i(LOGTAG, "Configuración correcta");
-                        startLocationUpdates();
-
-                        break;
-                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                        try {
-                            Log.i(LOGTAG, "Se requiere actuación del usuario");
-                            status.startResolutionForResult(MainActivity.this, PETICION_CONFIG_UBICACION);
-                        } catch (IntentSender.SendIntentException e) {
-                            btnActualizar.setChecked(false);
-                            Log.i(LOGTAG, "Error al intentar solucionar configuración de ubicación");
-                        }
-
-                        break;
-                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                        Log.i(LOGTAG, "No se puede cumplir la configuración de ubicación necesaria");
-                        btnActualizar.setChecked(false);
-                        break;
-                }
-            }
-        });
-    }
-
-    private void disableLocationUpdates() {
-
-        LocationServices.FusedLocationApi.removeLocationUpdates(
-                apiClient, this);
-
-    }
-
-    private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
-            //Ojo: estamos suponiendo que ya tenemos concedido el permiso.
-            //Sería recomendable implementar la posible petición en caso de no tenerlo.
-
-            Log.i(LOGTAG, "Inicio de recepción de ubicaciones");
-
-            LocationServices.FusedLocationApi.requestLocationUpdates(
-                    apiClient, locRequest, MainActivity.this);
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        //Se ha producido un error que no se puede resolver automáticamente
-        //y la conexión con los Google Play Services no se ha establecido.
-
-        Log.e(LOGTAG, "Error grave al conectar con Google Play Services");
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        //Conectado correctamente a Google Play Services
-
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PETICION_PERMISO_LOCALIZACION);
-        } else {
-
-            Location lastLocation =
-                    LocationServices.FusedLocationApi.getLastLocation(apiClient);
-
-            updateUI(lastLocation);
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        //Se ha interrumpido la conexión con Google Play Services
-
-        Log.e(LOGTAG, "Se ha interrumpido la conexión con Google Play Services");
-    }
-
-    private void updateUI(Location loc) {
-        if (loc != null) {
-            lblLatitud.setText("Latitud: " + String.valueOf(loc.getLatitude()));
-            lblLongitud.setText("Longitud: " + String.valueOf(loc.getLongitude()));
-            lat=String.valueOf(loc.getLatitude());
-            lon= String.valueOf(loc.getLongitude());
-            new mandarLoc().execute();
-        } else {
-            lblLatitud.setText("Latitud: (desconocida)");
-            lblLongitud.setText("Longitud: (desconocida)");
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == PETICION_PERMISO_LOCALIZACION) {
-            if (grantResults.length == 1
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                //Permiso concedido
-
-                @SuppressWarnings("MissingPermission")
-                Location lastLocation =
-                        LocationServices.FusedLocationApi.getLastLocation(apiClient);
-
-                updateUI(lastLocation);
-
-            } else {
-                //Permiso denegado:
-                //Deberíamos deshabilitar toda la funcionalidad relativa a la localización.
-
-                Log.e(LOGTAG, "Permiso denegado");
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case PETICION_CONFIG_UBICACION:
-                switch (resultCode) {
-                    case Activity.RESULT_OK:
-                        startLocationUpdates();
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        Log.i(LOGTAG, "El usuario no ha realizado los cambios de configuración necesarios");
-                        btnActualizar.setChecked(false);
-                        break;
-                }
-                break;
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        Log.i(LOGTAG, "Recibida nueva ubicación!");
-
-        //Mostramos la nueva ubicación recibida
-        updateUI(location);
     }
 
 
-    class mandarLoc extends AsyncTask<String, String, String> {
+    public void abrirMapa(View view){
+        Intent intent = new Intent(MainActivity.this, FragmentPosicion.class);
+        startActivity(intent);
 
-        /**
-         * Before starting background thread Show Progress Dialog
-         * */
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+    }
 
-        }
+    public void abrirChat(View view){
 
-        /**
-         * getting Albums JSON
-         * */
-        protected String doInBackground(String... args) {
-            // Building Parameters
-            //add your data
-            //add your data
-            JSONParse jsp= new JSONParse();
+    }
 
-            //String body= "{\r\n\"contrasena\": \""+txtPass.getText()+"\",\r\n\"telefono\": \""+txtTel.getText()+"\"\r\n}\r\n";
-            String body= "{\n\"id\" : \"3\",\n\"lat\" : \""+lat+"\",\n\"lon\" : \""+lon+"\"\n}";
-            String respuesta= jsp.makeHttpRequest("http://thea.devworms.com/api/usuarios/trackup","POST",body,"");
-            Log.d("Respuesta : ", "> " + respuesta);
-            if(respuesta!="error") {
+    public void pedirAyuda(View view){
 
-
-            }
-            else{}
-            return null;
-        }
-
-
-        /**
-         * After completing background task Dismiss the progress dialog
-         * **/
-        protected void onPostExecute(String file_url) {
-            // dismiss the dialog after getting all albums
-
-
-        }
     }
 }
